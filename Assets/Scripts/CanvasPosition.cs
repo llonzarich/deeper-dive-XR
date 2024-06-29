e1e6d1102a6f68e5c488f5ddb25f53db9b6e3686
Lonzarich <lydial1@iastate.edu>
2024-06-28 19:02:57 -0500

    popup placement works & text box function for object

---
+++ Assets/Scripts/CanvasPosition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CanvasPosition : MonoBehaviour
+{
+
+    [SerializeField] private GameObject camera,PanelText;
+
+    [SerializeField] private int distance;
+
+    public void placeCanvas()
+    {
+        this.transform.position = camera.transform.position + camera.transform.forward * distance;
+    }
+
+    public void assignText(string text)
+    {
+        TMP_Text mytext = PanelText.GetComponent<TMP_Text>();
+        mytext.text = text;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
